8c759a4b3dc2774dbc5cdcc5be04a26a9f1dd9dc
MikhailLebedev <lebedev.m1997@mail.ru>
2017-03-23 01:09:36 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -10,6 +10,9 @@
 #include <cmath>
 #include <cstdio>
 
+#define DEF_MOMENT 1
+#define DEF_SPEED 1
+
 enum{
     OUT_L = 1,
     HID_L = 0
@@ -36,85 +39,77 @@ double summ(double *signal, double *weight, int n){
     return sum;
 }
 
+class Network{
+    double ***weight;
+    double ***dweight;
+    double **output;
+    int *neurons;
+    int layers;
+    double moment;
+    double speed;
+public:
+    Network(int l, int *n, double m = DEF_MOMENT, double s = DEF_SPEED){
+        layers = l;
+        moment = m;
+        speed = s;
+        neurons = new int [layers];
+        while(--l >= 0){
+            neurons[l] = n[l];
+        }
+    }
+    ~Network(){}
+    void learn(double *input, double *solution){
+        double *answer = solve(normalize(input));
+        backpropagation(solution);
+    }
+    double *solve(double *input) const {
 
-void backpropagation(double ***weight, double **output, double *solution, int *neurons, int layers, double moment, double speed)
-{
-    //current layer index
-    int cli = layers - 1;
-    double sum;
-    //allocting deltas array
-    double **delta = new double* [layers];
-    for (int i = 0; i < layers; ++i){
-        delta[i] = new double [neurons[i]];
     }
-    //computing deltal on output layer
-    for (int i = 0; i < neurons[cli]; ++i){
-        delta[cli][i] = -2 * moment * output[cli][i] * (1 - output[cli][i]) * (solution[i] - output[cli][i]);
-    }
-    //computing deltas on hidden layers
-    while (--cli){
-        for (int i = 0; i < neurons[cli]; ++i){
-            sum = 0;
-            for (int j = 0; j < neurons[cli + 1]; ++j){
-                sum += delta[cli + 1][j] * weight[cli][j][i];
-            }
-            delta[cli][i] = 2 * moment * output[cli][i] * (1 - output[cli][i]) * sum;
+    double *normalize(double *input){
+        double *norm = new double [neurons[0]];
+        for (int i = 0; i < neurons[0]; ++i){
+            norm[i] = 1 / input[i];
         }
+        return norm;
     }
-    //recomputing weights
-    while(++cli < layers){
-        for (int i = 0; i < neurons[cli]; ++i){
-            for (int j = 0; j < neurons[cli - 1]; ++j){
-                weight[cli][i][j] -= speed * delta[cli][i] * output[cli - 1][j];
-            }
+    void backpropagation(double *solution){
+        //current layer index
+        int cli = layers - 1;
+        double sum;
+        //allocting deltas array
+        double **delta = new double* [layers];
+        for (int i = 0; i < layers; ++i){
+            delta[i] = new double [neurons[i]];
         }
-    }
-    // deleting memory of delta array
-    for (int i = 0; i < layers; ++i){
-        delete[] delta[i];
-    }
-    delete[] delta;
-}
-
-void backpropagation2(double ***weight, double ***dweight, double **output, double *solution, int *neurons, int layers, double moment, double speed)
-{
-    //current layer index
-    int cli = layers - 1;
-    double sum;
-    //allocting deltas array
-    double **delta = new double* [layers];
-    for (int i = 0; i < layers; ++i){
-        delta[i] = new double [neurons[i]];
-    }
-    //computing deltal on output layer
-    for (int i = 0; i < neurons[cli]; ++i){
-        delta[cli][i] = -1 * output[cli][i] * (1 - output[cli][i]) * (solution[i] - output[cli][i]);
-    }
-    //computing deltas on hidden layers
-    while (--cli){
+        //computing deltal on output layer
         for (int i = 0; i < neurons[cli]; ++i){
-            sum = 0;
-            for (int j = 0; j < neurons[cli + 1]; ++j){
-                sum += delta[cli + 1][j] * weight[cli][j][i];
-            }
-            delta[cli][i] = output[cli][i] * (1 - output[cli][i]) * sum;
+            delta[cli][i] = -1 * output[cli][i] * (1 - output[cli][i]) * (solution[i] - output[cli][i]);
         }
-    }
-    //recomputing weights
-    while(++cli < layers){
-        for (int i = 0; i < neurons[cli]; ++i){
-            for (int j = 0; j < neurons[cli - 1]; ++j){
-                weight[cli][i][j] += moment * dweight[cli][i][j] + (1 - moment) * speed * delta[cli][i] * output[cli - 1][j];
+        //computing deltas on hidden layers
+        while (--cli){
+            for (int i = 0; i < neurons[cli]; ++i){
+                sum = 0;
+                for (int j = 0; j < neurons[cli + 1]; ++j){
+                    sum += delta[cli + 1][j] * weight[cli][j][i];
+                }
+                delta[cli][i] = output[cli][i] * (1 - output[cli][i]) * sum;
             }
         }
+        //recomputing weights
+        while(++cli < layers){
+            for (int i = 0; i < neurons[cli]; ++i){
+                for (int j = 0; j < neurons[cli - 1]; ++j){
+                    weight[cli][i][j] += moment * dweight[cli][i][j] + (1 - moment) * speed * delta[cli][i] * output[cli - 1][j];
+                }
+            }
+        }
+        // deleting memory of delta array
+        for (int i = 0; i < layers; ++i){
+            delete[] delta[i];
+        }
+        delete[] delta;
     }
-    // deleting memory of delta array
-    for (int i = 0; i < layers; ++i){
-        delete[] delta[i];
-    }
-    delete[] delta;
-}
-
+};
 
 
 int main()
